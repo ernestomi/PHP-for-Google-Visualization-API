704d90edca4980798eb8a7c6635597006ef1bcbb
ernestomonroy <ehmizmg@gmail.com>
2016-04-11 07:42:30 +0100

    Update sampleSQLtoGoogle.php

--- sampleSQLtoGoogle.php
+++ sampleSQLtoGoogle.php
@@ -1,5 +1,5 @@
 <?php
-	include '../php/googleJSONconverter.php';
+	include 'googleJSONconverter.php';
 	$SQLString= "SELECT 'HELLO WORLD' STRING_COLUMN, 1234 NUMBER_COLUMN, SYSDATE DATE_COLUMN FROM DUAL";
 	$db = '(DESCRIPTION=(CID=DCA)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521)))(CONNECT_DATA=(SID=DCA)))' ;
 	$un = 'DCA_WEB' ;
